ba2cc1b9cf0e77b15c5cba2481275bdc08e00026
Софья Копейкина SK-5 <sofuy556@gmail.com>
2022-09-25 21:47:27 +0300

    Complete task3

--- 2022/practice3/main.cpp
+++ 2022/practice3/main.cpp
@@ -13,6 +13,8 @@
 #include <chrono>
 #include <vector>
 
+#include "series_n_units.hpp"
+
 std::string to_string(std::string_view str)
 {
     return std::string(str.begin(), str.end());
@@ -25,115 +27,8 @@ void sdl2_fail(std::string_view message)
 
 void glew_fail(std::string_view message, GLenum error)
 {
-    throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
-}
-
-const char vertex_shader_source[] =
-R"(#version 330 core
-
-uniform mat4 view;
-
-layout (location = 0) in vec2 in_position;
-layout (location = 1) in vec4 in_color;
-
-out vec4 color;
-
-void main()
-{
-    gl_Position = view * vec4(in_position, 0.0, 1.0);
-    color = in_color;
-}
-)";
-
-const char fragment_shader_source[] =
-R"(#version 330 core
-
-in vec4 color;
-
-layout (location = 0) out vec4 out_color;
-
-void main()
-{
-    out_color = color;
-}
-)";
-
-GLuint create_shader(GLenum type, const char * source)
-{
-    GLuint result = glCreateShader(type);
-    glShaderSource(result, 1, &source, nullptr);
-    glCompileShader(result);
-    GLint status;
-    glGetShaderiv(result, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE)
-    {
-        GLint info_log_length;
-        glGetShaderiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
-        std::string info_log(info_log_length, '\0');
-        glGetShaderInfoLog(result, info_log.size(), nullptr, info_log.data());
-        throw std::runtime_error("Shader compilation failed: " + info_log);
-    }
-    return result;
-}
-
-GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
-{
-    GLuint result = glCreateProgram();
-    glAttachShader(result, vertex_shader);
-    glAttachShader(result, fragment_shader);
-    glLinkProgram(result);
-
-    GLint status;
-    glGetProgramiv(result, GL_LINK_STATUS, &status);
-    if (status != GL_TRUE)
-    {
-        GLint info_log_length;
-        glGetProgramiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
-        std::string info_log(info_log_length, '\0');
-        glGetProgramInfoLog(result, info_log.size(), nullptr, info_log.data());
-        throw std::runtime_error("Program linkage failed: " + info_log);
-    }
-
-    return result;
-}
-
-struct vec2
-{
-    float x;
-    float y;
-};
-
-struct vertex
-{
-    vec2 position;
-    std::uint8_t color[4];
-};
-
-vec2 bezier(std::vector<vertex> const & vertices, float t)
-{
-    std::vector<vec2> points(vertices.size());
-
-    for (std::size_t i = 0; i < vertices.size(); ++i)
-        points[i] = vertices[i].position;
-
-    // De Casteljau's algorithm
-    for (std::size_t k = 0; k + 1 < vertices.size(); ++k) {
-        for (std::size_t i = 0; i + k + 1 < vertices.size(); ++i) {
-            points[i].x = points[i].x * (1.f - t) + points[i + 1].x * t;
-            points[i].y = points[i].y * (1.f - t) + points[i + 1].y * t;
-        }
-    }
-    return points[0];
-}
-
-void AttributeStructure() {
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
-        sizeof(vertex), (void *)(0));
-    
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE,
-        sizeof(vertex), (void *)(8));
+    throw std::runtime_error(to_string(message) +
+        reinterpret_cast<const char *>(glewGetErrorString(error)));
 }
 
 int main() try
@@ -159,6 +54,7 @@ int main() try
 
     int width, height;
     SDL_GetWindowSize(window, &width, &height);
+    series::resize(width, height);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     if (!gl_context)
@@ -174,40 +70,13 @@ int main() try
 
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
-    auto vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_shader_source);
-    auto fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
-    auto program = create_program(vertex_shader, fragment_shader);
-
-    GLuint view_location = glGetUniformLocation(program, "view");
-
     auto last_frame_start = std::chrono::high_resolution_clock::now();
 
-    float time = 0.f, aspect_ratio = float(width) / height;
-
-    vertex points[3] = {
-        {{112, 140}, {128, 0, 0, 255}},
-        {{130, 170}, {0, 128, 255, 255}},
-        {{123, 157}, {128, 0, 128, 255}}
-    };
-
-    // vao & vbo
-    GLuint buffers[2];
-    glGenVertexArrays(1, buffers);
-    glGenBuffers(1, buffers + 1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
-    AttributeStructure();
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * 3, points, GL_STATIC_DRAW);
-
-    /*
-    vertex puk;
-    glGetBufferSubData(GL_ARRAY_BUFFER, 2 * sizeof(vertex), sizeof(vertex), &puk);
-
-    std::cout << puk.position.x << " " << puk.position.y << " "
-        << puk.color[0] << "." << puk.color[1] << "." << puk.color[2] << std::endl;
-    */
+    float time = 0.f;
 
     bool running = true;
+
+    series *obj = new bezier();
     while (running)
     {
         for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
@@ -221,7 +90,7 @@ int main() try
                 width = event.window.data1;
                 height = event.window.data2;
                 glViewport(0, 0, width, height);
-                aspect_ratio = float(width) / height;
+                series::resize(width, height);
                 break;
             }
             break;
@@ -230,20 +99,21 @@ int main() try
             {
                 int mouse_x = event.button.x;
                 int mouse_y = event.button.y;
+                obj->mouse_update(mouse_x, mouse_y);
             }
             else if (event.button.button == SDL_BUTTON_RIGHT)
             {
-
+                obj->mouse_update();
             }
             break;
         case SDL_KEYDOWN:
             if (event.key.keysym.sym == SDLK_LEFT)
             {
-
+                obj->key_update();
             }
             else if (event.key.keysym.sym == SDLK_RIGHT)
             {
-
+                obj->key_update(true);
             }
             break;
         }
@@ -256,31 +126,15 @@ int main() try
         last_frame_start = now;
         time += dt;
 
+        series::timer(time);
+
         glClear(GL_COLOR_BUFFER_BIT);
 
-        float view[16] =
-        {
-            1.0 / aspect_ratio,    0.f, 0.f,   -1.0 / width,
-            0.f, - 1.0 / height, 0.f, -1.0 / height,
-            0.f, 0.f,            1.f,           0.f,
-            0.f, 0.f,            0.f,           1.f
-        };
-
-        glUseProgram(program);
-
-        glBindVertexArray(buffers[0]);
-
-        glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
-        AttributeStructure();
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * 3, points, GL_STATIC_DRAW);
-
-        glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        obj->draw();
 
         SDL_GL_SwapWindow(window);
     }
-
+    delete obj;
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
 }
