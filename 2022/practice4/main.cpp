3570f7ee430921ef240796a72d82b198fcf1904c
Софья Копейкина SK-5 <sofuy556@gmail.com>
2022-10-03 01:44:28 +0300

    все пошло к хуям

--- 2022/practice4/main.cpp
+++ 2022/practice4/main.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 #include <map>
 
-#include "obj_parser.hpp"
+#include "series_n_units.hpp"
 
 std::string to_string(std::string_view str)
 {
@@ -29,77 +29,6 @@ void sdl2_fail(std::string_view message)
 void glew_fail(std::string_view message, GLenum error)
 {
     throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
-}
-
-const char vertex_shader_source[] =
-R"(#version 330 core
-
-uniform mat4 view;
-uniform mat4 transform;
-
-layout (location = 0) in vec3 in_position;
-layout (location = 1) in vec3 in_normal;
-
-out vec3 normal;
-
-void main()
-{
-    gl_Position = view * transform * vec4(in_position, 1.0);
-    normal = mat3(transform) * in_normal;
-}
-)";
-
-const char fragment_shader_source[] =
-R"(#version 330 core
-
-in vec3 normal;
-
-layout (location = 0) out vec4 out_color;
-
-void main()
-{
-    float lightness = 0.5 + 0.5 * dot(normalize(normal), normalize(vec3(1.0, 2.0, 3.0)));
-    out_color = vec4(vec3(lightness), 1.0);
-}
-)";
-
-GLuint create_shader(GLenum type, const char * source)
-{
-    GLuint result = glCreateShader(type);
-    glShaderSource(result, 1, &source, nullptr);
-    glCompileShader(result);
-    GLint status;
-    glGetShaderiv(result, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE)
-    {
-        GLint info_log_length;
-        glGetShaderiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
-        std::string info_log(info_log_length, '\0');
-        glGetShaderInfoLog(result, info_log.size(), nullptr, info_log.data());
-        throw std::runtime_error("Shader compilation failed: " + info_log);
-    }
-    return result;
-}
-
-GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
-{
-    GLuint result = glCreateProgram();
-    glAttachShader(result, vertex_shader);
-    glAttachShader(result, fragment_shader);
-    glLinkProgram(result);
-
-    GLint status;
-    glGetProgramiv(result, GL_LINK_STATUS, &status);
-    if (status != GL_TRUE)
-    {
-        GLint info_log_length;
-        glGetProgramiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
-        std::string info_log(info_log_length, '\0');
-        glGetProgramInfoLog(result, info_log.size(), nullptr, info_log.data());
-        throw std::runtime_error("Program linkage failed: " + info_log);
-    }
-
-    return result;
 }
 
 int main() try
@@ -142,21 +71,13 @@ int main() try
 
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
-    auto vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_shader_source);
-    auto fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
-    auto program = create_program(vertex_shader, fragment_shader);
-
-    GLuint view_location = glGetUniformLocation(program, "view");
-    GLuint transform_location = glGetUniformLocation(program, "transform");
-
-    std::string project_root = PROJECT_ROOT;
-    obj_data bunny = parse_obj(project_root + "/bunny.obj");
-
     auto last_frame_start = std::chrono::high_resolution_clock::now();
 
     float time = 0.f;
 
     std::map<SDL_Keycode, bool> button_down;
+
+    series *obj = new bunny();
 
     bool running = true;
     while (running)
@@ -172,6 +93,7 @@ int main() try
                 width = event.window.data1;
                 height = event.window.data2;
                 glViewport(0, 0, width, height);
+                obj->resize(width, height);
                 break;
             }
             break;
@@ -193,28 +115,12 @@ int main() try
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        float view[16] =
-        {
-            1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f,
-        };
-
-        float transform[16] =
-        {
-            1.f, 0.f, 0.f, 0.f,
-            0.f, 1.f, 0.f, 0.f,
-            0.f, 0.f, 1.f, 0.f,
-            0.f, 0.f, 0.f, 1.f,
-        };
-
-        glUseProgram(program);
-        glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
-        glUniformMatrix4fv(transform_location, 1, GL_TRUE, transform);
+        obj->draw();
 
         SDL_GL_SwapWindow(window);
     }
+
+    delete obj;
 
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
