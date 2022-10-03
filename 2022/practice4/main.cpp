50d6228bfbeb69e8b477abb367c0b87de24c2a85
Софья Копейкина SK-5 <sofuy556@gmail.com>
2022-10-03 13:48:08 +0300

    complete 4 task

--- 2022/practice4/main.cpp
+++ 2022/practice4/main.cpp
@@ -73,12 +73,21 @@ int main() try
 
     auto last_frame_start = std::chrono::high_resolution_clock::now();
 
-    float time = 0.f;
+    float time = 0.f, tmp = 0.f;
 
     std::map<SDL_Keycode, bool> button_down;
 
-    series *obj = new bunny();
+    series *obj[] = { 
+        new bunny(-2, 2, {0, 1, 4, 5}),
+        new bunny(-2, -2, {0, 2, 8, 10}),
+        new bunny(2, 2, {5, 6, 9, 10}),
+    };
 
+
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
     bool running = true;
     while (running)
     {
@@ -93,7 +102,7 @@ int main() try
                 width = event.window.data1;
                 height = event.window.data2;
                 glViewport(0, 0, width, height);
-                obj->resize(width, height);
+                series::resize(width, height);
                 break;
             }
             break;
@@ -113,14 +122,39 @@ int main() try
         last_frame_start = now;
         time += dt;
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        if (button_down[SDLK_UP])
+            for (int i = 0; i < 3; ++i)
+                obj[i]->key_update(+1, dt, true);
+        else if (button_down[SDLK_DOWN])
+            for (int i = 0; i < 3; ++i)
+                obj[i]->key_update(-1, dt, true);
 
-        obj->draw();
+        if (button_down[SDLK_LEFT])
+            for (int i = 0; i < 3; ++i)
+                obj[i]->key_update(-1, dt);
+        else if (button_down[SDLK_RIGHT])
+            for (int i = 0; i < 3; ++i)
+                obj[i]->key_update(+1, dt);
+
+        series::timer(time);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //XZ
+        obj[1]->draw();
+
+        // XY
+        obj[0]->draw();
+
+        // YZ
+        glCullFace(GL_BACK);
+        obj[2]->draw();
 
         SDL_GL_SwapWindow(window);
     }
 
-    delete obj;
+    for (int i = 0; i < 3; ++i)
+        delete obj[i];
 
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
